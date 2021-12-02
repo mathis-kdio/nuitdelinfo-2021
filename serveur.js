f3334f2f7ba9ec8a47ce4e945623dd4775bb4c79
Mathis Cadio <cadio@et.esiea.fr>
2021-12-03 00:38:49 +0100

    Ajout description bateau

--- serveur.js
+++ serveur.js
@@ -50,13 +50,13 @@ app.get('/bateaux', function (req, res) {
 app.get('/bateaux/:type', function (req, res) {
     let type = req.params.type;
     if (type == 'all') {
-        connection.query("SELECT id, nom, type, annee_debut FROM bateaux", function (err, result) {
+        connection.query("SELECT id, nom, type, annee_debut, description FROM bateaux", function (err, result) {
             if (err) throw (err);
             res.send(result);
         })
     }
     else {
-        connection.query("SELECT id, nom, type, annee_debut FROM bateaux WHERE type=" + type, function (err, result) {
+        connection.query("SELECT id, nom, type, annee_debut, description FROM bateaux WHERE type=" + type, function (err, result) {
             if (err) throw (err);
             res.send(result);
         })

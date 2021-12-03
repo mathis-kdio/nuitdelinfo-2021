f3bcfdb816e08ca6bceed352bd1393efef9a5448
Mathis Cadio <cadio@et.esiea.fr>
2021-12-03 01:54:25 +0100

    Backend get sortie en mer by id

--- serveur.js
+++ serveur.js
@@ -109,6 +109,13 @@ app.get('/sorties-en-mer', function (req, res) {
     })
 });
 
+app.get('/sorties-en-mer/:id', function (req, res) {
+    connection.query("SELECT sem.id, v.nom AS ville, date, sa.nom AS commandant, succes, morts, si.nom AS siecle FROM sorties_en_mer AS sem JOIN siecle AS si ON si.id = sem.siecle JOIN villes AS v ON v.id = sem.ville JOIN sauveteurs AS sa ON sa.id = sem.commandant WHERE sem.id=" + req.params.id, function (err, result) {
+        if (err) throw (err);
+        res.send(result);
+    })
+});
+
 app.get('/sorties-en-mer/siecle/:IdSiecle', function (req, res) {
     connection.query("SELECT sem.id, v.nom AS ville, date, sa.nom AS commandant, succes, morts, si.nom AS siecle FROM sorties_en_mer AS sem JOIN siecle AS si ON si.id = sem.siecle JOIN villes AS v ON v.id = sem.ville JOIN sauveteurs AS sa ON sa.id = sem.commandant WHERE si.id=" + req.params.IdSiecle, function (err, result) {
         if (err) throw (err);

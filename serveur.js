8d458920bf894baeba0c558f4be9f9ca2769c991
Mathis Cadio <cadio@et.esiea.fr>
2021-12-03 01:28:47 +0100

    Fix id query sortie en mer

--- serveur.js
+++ serveur.js
@@ -105,13 +105,13 @@ app.get('/stations/:id', function (req, res) {
 app.get('/sorties-en-mer/:type', function (req, res) {
     let type = req.params.type;
     if (type == 'all') {
-        connection.query("SELECT id, v.nom AS ville, date, sa.nom AS commandant, succes, morts, si.nom AS siecle FROM sorties_en_mer AS sem JOIN siecle AS si ON si.id = sem.siecle JOIN villes AS v ON v.id = sem.ville JOIN sauveteurs AS sa ON sa.id = sem.commandant", function (err, result) {
+        connection.query("SELECT sem.id, v.nom AS ville, date, sa.nom AS commandant, succes, morts, si.nom AS siecle FROM sorties_en_mer AS sem JOIN siecle AS si ON si.id = sem.siecle JOIN villes AS v ON v.id = sem.ville JOIN sauveteurs AS sa ON sa.id = sem.commandant", function (err, result) {
             if (err) throw (err);
             res.send(result);
         })
     }
     else {
-        connection.query("SELECT id, v.nom AS ville, date, sa.nom AS commandant, succes, morts, si.nom AS siecle FROM sorties_en_mer AS sem JOIN siecle AS si ON si.id = sem.siecle JOIN villes AS v ON v.id = sem.ville JOIN sauveteurs AS sa ON sa.id = sem.commandant WHERE siecle=" + type, function (err, result) {
+        connection.query("SELECT sem.id, v.nom AS ville, date, sa.nom AS commandant, succes, morts, si.nom AS siecle FROM sorties_en_mer AS sem JOIN siecle AS si ON si.id = sem.siecle JOIN villes AS v ON v.id = sem.ville JOIN sauveteurs AS sa ON sa.id = sem.commandant WHERE siecle=" + type, function (err, result) {
             if (err) throw (err);
             res.send(result);
         })
